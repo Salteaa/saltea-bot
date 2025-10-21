51c82a823d4b49452a9cee545c9604a1caea466c
Saltea <salteaaa@gmail.com>
2025-10-20 19:37:19 -0600

    ur mom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import discord
 from discord.ext import commands
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
+discord_token = os.getenv("DISCORD_TOKEN")
 intents = discord.Intents.default()
 intents.message_content = True  # Required for reading messages
 
@@ -14,4 +18,4 @@ async def on_ready():
 async def ping(ctx):
     await ctx.send('Pong!')
 
-bot.run("")
+bot.run(discord_token)
